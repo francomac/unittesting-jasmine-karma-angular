30e70316d89b44bb112d188fdc7f375d411ca7f5
Franco Martinez Carballo <franco.mcarballo@outlook.com>
2020-05-05 21:22:00 -0600

    concepts: dependency injection, testbed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,24 +1,42 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService', () => {
 
+    let calculator: CalculatorService, loggerSpy: any;
+
+    beforeEach( () => {
+
+        console.log('calling beforeEach');
+
+        loggerSpy = jasmine.createSpyObj('loggerService', ['log']);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+
+        calculator = TestBed.get(CalculatorService);
+
+    });
+
     it('shoud add two numbers', () => {
-        const logger = jasmine.createSpyObj('loggerService', ['log']);
 
-        const calculator = new CalculatorService(logger);
-
+        console.log('calling addTest');
+        
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
 
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
     it('shoud substract two numbers', () => {
-        const logger = jasmine.createSpyObj('loggerService', ['log']);
 
-        const calculator = new CalculatorService(logger);
+        console.log('calling substractTest');
 
         const result = calculator.subtract(2, 2);
 
